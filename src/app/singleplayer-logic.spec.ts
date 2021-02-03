7543867d56445308bb06bbe117e9d0df41c050e0
Sebastian Matyszewski <sebastianmat1@tlen.pl>
2021-02-03 06:06:17 +0100

    dodanie wyglądu

--- src/app/singleplayer-logic.spec.ts
+++
@@ -1,7 +0,0 @@
-import { SingleplayerLogic } from './singleplayer-logic';
-
-describe('SingleplayerLogic', () => {
-  it('should create an instance', () => {
-    expect(new SingleplayerLogic()).toBeTruthy();
-  });
-});
